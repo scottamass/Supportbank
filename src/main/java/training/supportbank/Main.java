8614a3d3293cdc8e1f8f7434b673358cfbe155a4
scottamass <scot.atkinson@live.co.uk>
2021-11-19 15:16:16 +0000

    first commit

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String args[]) {
         // Your code here!
         System.out.println("Test!");
+
+        void listALL {
+
+        }
+
+        void list{
+
+        }
     }
 }
